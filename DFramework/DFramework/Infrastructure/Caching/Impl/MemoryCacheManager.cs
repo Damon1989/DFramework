d752e331ac2980cf4d9cb212ae74410da3cd0d4e
Damon1989 <1015080921@qq.com>
2018-05-13 20:49:41 +0800

    缓存实现

---
+++ DFramework/DFramework/Infrastructure/Caching/Impl/MemoryCacheManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DFramework.Infrastructure.Caching.Impl
+{
+    /// <summary>
+    /// Represents a manager for caching between HTTP requests (long term caching)
+    /// </summary>
+    public class MemoryCacheManager : CacheManagerBase
+    {
+        protected ObjectCache Cache => MemoryCache.Default;
+
+        public override CacheValue<T> Get<T>(string key)
+        {
+            return Cache[key] as CacheValue<T> ?? CacheValue<T>.NoValue;
+        }
+
+        public override void Set<T>(string key, T data, int cacheTime)
+        {
+            var policy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTime.Now + TimeSpan.FromMinutes(cacheTime)
+            };
+            Cache.Add(new CacheItem(key, new CacheValue<T>(data, true)), policy);
+        }
+
+        public override bool IsSet(string key)
+        {
+            return Cache.Contains(key);
+        }
+
+        public override void Remove(string key)
+        {
+            Cache.Remove(key);
+        }
+
+        public override void RemoveByPattern(string pattern)
+        {
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var keysToRemove = new List<string>();
+
+            foreach (var item in Cache)
+            {
+                if (regex.IsMatch(item.Key))
+                {
+                    keysToRemove.Add(item.Key);
+                }
+            }
+
+            foreach (var key in keysToRemove)
+            {
+                Remove(key);
+            }
+        }
+
+        public override void Clear()
+        {
+            foreach (var item in Cache)
+            {
+                Remove(item.Key);
+            }
+        }
+    }
+}
---
+++ DFramework/DFramework/Infrastructure/Caching/Impl/PerHttpRequestCacheManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DFramework.Infrastructure.Caching.Impl
+{
+    public class PerHttpRequestCacheManager : CacheManagerBase
+    {
+        private readonly HttpContextBase _context;
+
+        public PerHttpRequestCacheManager(HttpContextBase context)
+        {
+            _context = context;
+        }
+
+        protected virtual IDictionary GetItems()
+        {
+            return _context?.Items;
+        }
+
+        public override CacheValue<T> Get<T>(string key)
+        {
+            var items = GetItems();
+            return items == null ? CacheValue<T>.NoValue : new CacheValue<T>((T)items[key], true);
+        }
+
+        public override void Set<T>(string key, T data, int cacheTime)
+        {
+            var items = GetItems();
+            if (items == null)
+            {
+                return;
+            }
+
+            if (items.Contains(key))
+            {
+                items[key] = data;
+            }
+            else
+            {
+                items.Add(key, data);
+            }
+        }
+
+        public override bool IsSet(string key)
+        {
+            var items = GetItems();
+            return items?[key] != null;
+        }
+
+        public override void Remove(string key)
+        {
+            var items = GetItems();
+
+            items?.Remove(key);
+        }
+
+        public override void RemoveByPattern(string pattern)
+        {
+            var items = GetItems();
+            if (items == null)
+            {
+                return;
+            }
+
+            var enumerator = items.GetEnumerator();
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var keysToRemove = new List<string>();
+            while (enumerator.MoveNext())
+            {
+                if (regex.IsMatch(enumerator.Key.ToString()))
+                {
+                    keysToRemove.Add(enumerator.Key.ToString());
+                }
+            }
+
+            foreach (var key in keysToRemove)
+            {
+                items.Remove(key);
+            }
+        }
+
+        public override void Clear()
+        {
+            var items = GetItems();
+            if (items == null)
+            {
+                return;
+            }
+
+            var enumerator = items.GetEnumerator();
+            var keysToRemove = new List<string>();
+            while (enumerator.MoveNext())
+            {
+                if (enumerator.Key != null)
+                {
+                    keysToRemove.Add(enumerator.Key.ToString());
+                }
+            }
+
+            foreach (var key in keysToRemove)
+            {
+                items.Remove(key);
+            }
+        }
+    }
+}
