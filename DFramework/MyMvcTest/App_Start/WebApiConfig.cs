c0b1e0e2b456cde3899636c70ab2e1284117a216
Damon1989 <1015080921@qq.com>
2020-04-28 09:04:58 +0800

    code clean up

--- DFramework/MyMvcTest/App_Start/WebApiConfig.cs
+++ DFramework/MyMvcTest/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace MyMvcTest
@@ -9,6 +11,21 @@ namespace MyMvcTest
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            // Web API configuration and services
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("json", "true", "application/json"));
+
+            config.Formatters.JsonFormatter.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.XmlFormatter
+                .MediaTypeMappings
+                .Add(new QueryStringMapping("xml", "true", "application/xml"));
+
+
+            config.EnableCors();
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
